7c1242b70cf1fa252438861bb3062960fe7cc4d9
alvira77 <alviraarisa98@gmail.com>
2019-11-28 23:24:38 +0700

    Commit Hari Ke-7

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,6 +7,59 @@ var bullets = require('bespoke-bullets');
 var hash = require('bespoke-hash');
 var extern = require('bespoke-extern');
 
+const DATA = [
+  {
+      img_url:'images/Bear_1.jpg',
+      title:'Beruang',
+      title_en:'Bear'
+  },
+  {
+      img_url:'images/Cat_1.jpg',
+      title:'Kucing',
+      title_en:'Cat'
+  },
+  {
+      img_url:'images/Chameleon_1.jpg',
+      title:'Bunglon',
+      title_en:'Chameleon'
+  },
+  {
+      img_url:'images/Chicken_1.jpg',
+      title:'Ayam',
+      title_en:'Chicken'
+  },
+  {
+    img_url:'images/Clam_1.jpg',
+    title:'Kerang',
+    title_en:'Clam'
+},
+{
+    img_url:'images/Deer_1.jpg',
+    title:'Rusa',
+    title_en:'Deer'
+},
+{
+    img_url:'images/Dog_1.jpg',
+    title:'Anjing',
+    title_en:'Dog'
+},
+{
+    img_url:'images/Dolphin_1.jpg',
+    title:'Lumba-Lumba',
+    title_en:'Dolphin'
+},
+{
+  img_url:'images/Duck_1.jpg',
+  title:'Bebek',
+  title_en:'Duck'
+},
+{
+  img_url:'images/Elephant_1.jpg',
+  title:'Gajah',
+  title_en:'Elephant'
+},
+]
+
 // Bespoke.js
 bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   classes(),
@@ -16,3 +69,12 @@ bespoke.from({ parent: 'article.deck', slides: 'section' }, [
   hash(),
   extern(bespoke)
 ]);
+
+//Scripts
+$(document).ready(function(){
+  //Start Button
+  $('.start-btn').on('click',function(){
+    $('.start-overlay').css('transform','scale(0)');
+    $(this).css('transform','scale(0)');
+  });
+})
